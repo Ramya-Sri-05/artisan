09e6a7c1aed497caaf82012d39551652d3c29885
Ramya <ramyasri91779@gmail.com>
2025-06-11 16:49:02 +0530

    added function

--- cart.js
+++ cart.js
@@ -1,6 +1,25 @@
-export const cart=[
+export const cart=[];
 
-
-
+export function updateCart(productid){
     
-];
+       let matching;
+
+       cart.forEach((cartItem) => {
+         if(productid === cartItem.productid){
+               matching=cartItem;
+         }
+
+       })
+
+       if(matching){
+         matching.quantity++;
+
+       }
+       else{
+         cart.push({
+         productid:productid,
+         quantity:1
+       })
+      
+      }
+}
