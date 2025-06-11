50dd390c8cefcaf956bb3b3a0fa56c2743250d94
Ramya <ramyasri91779@gmail.com>
2025-06-11 11:49:06 +0530

    cart quantity dom updated

---
+++ artisan.js
@@ -0,0 +1,69 @@
+import {products} from './products.js';
+import {cart} from './cart.js'
+
+let productHTML=' ';
+
+
+products.forEach((product)=>{
+    productHTML += `
+     <div class="product-list" >
+        <img src="${product.image}"  title="famous painting" alt="pretty girl">
+        <p>$${product.price}   <del>$${product.del}</del></p>
+        <p>famous painting by $$$$</p>
+        <div class="b"><button class="js-addtocart" data-product-id="${product.productid}">Add to Cart</button></div>
+     </div>`
+    
+});
+document.querySelector(".js-product-container")
+.innerHTML = productHTML;
+
+
+document.querySelectorAll('.js-addtocart')
+.forEach((button) => {
+      button.addEventListener('click',() => {
+       const productid=button.dataset.productId;
+
+       let matching;
+
+       cart.forEach((cartItem) => {
+         if(productid === cartItem.productid){
+               matching=cartItem;
+         }
+
+       })
+
+       if(matching){
+         matching.quantity++;
+
+       }
+       else{
+         cart.push({
+         productid:productid,
+         quantity:1
+       })
+      
+      }
+      let cartQuantity=0;
+
+      cart.forEach((cartItems) => {
+         cartQuantity+=cartItems.quantity;
+
+      });
+
+
+      document.querySelector(".js-cart-quantity")
+      .innerHTML = cartQuantity;
+      
+       console.log(cartQuantity);
+       console.log(cart)
+
+      })
+     
+
+     
+
+})
+
+
+
+
