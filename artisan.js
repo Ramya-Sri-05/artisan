db233de480ef4beff856ad289fe3ca2ab97a573c
Ramya <ramyasri91779@gmail.com>
2025-06-11 12:50:44 +0530

    added message displayed

--- artisan.js
+++ artisan.js
@@ -10,12 +10,32 @@ products.forEach((product)=>{
         <img src="${product.image}"  title="famous painting" alt="pretty girl">
         <p>$${product.price}   <del>$${product.del}</del></p>
         <p>famous painting by $$$$</p>
-        <div class="b"><button class="js-addtocart" data-product-id="${product.productid}">Add to Cart</button></div>
+         <div class="cart-added-msg css-added-msg"></div>
+        <div class="cartbutton"><button class="js-addtocart" data-product-id="${product.productid}">Add to Cart</button></div>
      </div>`
     
 });
 document.querySelector(".js-product-container")
 .innerHTML = productHTML;
+
+
+   document.querySelectorAll('.js-addtocart')
+   .forEach((cartbuttton) => {
+      cartbuttton.addEventListener('click',()=>{
+     let a= document.querySelectorAll('.cart-added-msg');
+     a[parseInt(cartbuttton.dataset.productId)-1].innerHTML = '✅ added'
+
+      setTimeout(()=>{
+      a[parseInt(cartbuttton.dataset.productId)-1].innerHTML = ''
+   },2000)
+     
+      
+   })
+      
+   })
+
+  
+   
 
 
 document.querySelectorAll('.js-addtocart')
@@ -53,7 +73,7 @@ document.querySelectorAll('.js-addtocart')
 
       document.querySelector(".js-cart-quantity")
       .innerHTML = cartQuantity;
-      
+
        console.log(cartQuantity);
        console.log(cart)
 
