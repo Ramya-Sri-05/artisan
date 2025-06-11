ae9b0a636ace8342d97f0d25e3ab92d160c33d16
Ramya <ramyasri91779@gmail.com>
2025-06-11 11:32:04 +0530

    added cart feature

--- products.js
+++ products.js
@@ -1,8 +1,53 @@
-const products=[
-    {
-        
-    }
+ 
+export const products=[
+{
+    productid:1,
+    image:'artisan-pics/pic1.html.jpg',
+    del:30,
+    price:15
 
+},
+{
+     productid:2,
+    image:'artisan-pics/pic2.html.jpg',
+     del:40,
+    price:14
 
+},
+{
+     productid:3,
+    image:'artisan-pics/pic3.html.jpg',
+     del:40,
+    price:10
+
+},
+{
+     productid:4,
+    image:'artisan-pics/pic4.html.jpg',
+      del:40,
+    price:15
+
+},
+{
+     productid:5,
+    image:'artisan-pics/pic5.html.jpg',
+    del:40,
+    price:10
+
+},
+{
+     productid:6,
+    image:'artisan-pics/pic6.html.jpg',
+    del:40,
+    price:10
+
+},
+{
+     productid:7,
+    image:'artisan-pics/pic7.html.jpg',
+    del:40,
+    price:10
+
+}
 
 ]
